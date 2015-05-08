305e16b9c56a723b6b48a4c712fee0a80a69b667
chandresh <chandresh@costrategix.com>
2015-05-08 16:16:51 +0530

    added jacoco plugin and test

--- lab5/src/test/java/ca/uwo/csd/cs112/jshantz24/TestBankAccount.java
+++ lab5/src/test/java/ca/uwo/csd/cs112/jshantz24/TestBankAccount.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
+
   @Test 
   public void testDebitWithSufficientFunds() {
     BankAccount account = new BankAccount(10);
